bb77fefd8470b98229de8796059a9ae3900eaf9d
jennaescher <crara99@naver.com>
2023-08-24 16:44:10 +0900

    initial commit

---
+++ src/app/header.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from "react"
+import "../styles/globals.css";
+//import MainIcon from "/public/wanted.svg";
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <div>
+                        <div>
+                            <button></button>
+                            <a></a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default Header
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import "../styles/globals.css";
+import Header from "./header"
+
+
+export default function Home() {
+  return ( 
+    <Header></Header>
+  );
+}
