5e2e394805e3845b3a77bb870e5070fcd3d929c9
taemin-Jang <jtm990506@gmail.com>
2023-08-30 21:08:16 +0900

    Modify root layout, page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import Header from './components/header';
 import Carousel from './components/carousel';
+
 export default function Home() {
 	return (
-		<>
-			<Header />
-			<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-				<Carousel />
-				<div>
-					<Link href='/Login'>Login 이동</Link>
-					<br></br>
-					<Link href='/signup'>회원가입</Link>
-				</div>
-			</main>
-		</>
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<Carousel />
+			<div>
+				<Link href='/Login'>Login 이동</Link>
+				<br></br>
+				<Link href='/signup'>회원가입</Link>
+			</div>
+		</main>
 	);
 }
